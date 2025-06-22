e94adb4282080e99ee207919354e4ca96afd6aca
Developer Nahida <ummenahida20@gmail.com>
2025-06-22 22:16:25 +0600

    compile ts file

--- dist/app/models/booksRouter.js
+++ dist/app/models/booksRouter.js
@@ -1,76 +1,72 @@
-import express, { Request, Response } from 'express';
-import { borrowCollection } from '../contrololars/borrowControlar';
-import { booksCollections } from '../contrololars/booksControlars';
-
-export const booksRouters = express.Router();
-export const borrowRouters = express.Router();
-
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.borrowRouters = exports.booksRouters = void 0;
+const express_1 = __importDefault(require("express"));
+const borrowControlar_1 = require("../contrololars/borrowControlar");
+const booksControlars_1 = require("../contrololars/booksControlars");
+exports.booksRouters = express_1.default.Router();
+exports.borrowRouters = express_1.default.Router();
 // create a new book
-booksRouters.post('/api/books', async (req: Request, res: Response) => {
+exports.booksRouters.post('/api/books', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const body = req.body;
-        const books = new booksCollections(body);
-
-        await books.save();
-
+        const books = new booksControlars_1.booksCollections(body);
+        yield books.save();
         res.json({
             success: true,
             message: "Book created successfully",
             data: books
-        })
-
-
-    } catch (error) {
-
+        });
+    }
+    catch (error) {
         let errorName = 'Error';
-
         if (error && typeof error === 'object' && 'name' in error) {
-            errorName = (error as { name: string }).name;
+            errorName = error.name;
         }
-
         const errorResponse = {
             message: 'Something went wrong',
             success: false,
             error: {
                 name: errorName,
                 errors: error
-
             }
         };
-
-        res.json(errorResponse)
-
+        res.json(errorResponse);
     }
-
-})
-
+}));
 // get all books
-booksRouters.get('/api/books', async (req: Request, res: Response) => {
+exports.booksRouters.get('/api/books', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-
         const { filter, sortBy, sort, limit } = req.query;
-
         let books = [];
-
         if (filter || sortBy || sort || limit) {
-            books = await booksCollections.find({ genre: filter }).sort({ [sortBy as string]: sort === 'desc' ? -1 : 1 }).limit(Number(limit));
-
-        } else {
-            books = await booksCollections.find();
+            books = yield booksControlars_1.booksCollections.find({ genre: filter }).sort({ [sortBy]: sort === 'desc' ? -1 : 1 }).limit(Number(limit));
         }
-
+        else {
+            books = yield booksControlars_1.booksCollections.find();
+        }
         res.json({
             success: true,
             message: "Books retrieved successfully",
             data: books
-        })
-
-
-    } catch (error) {
+        });
+    }
+    catch (error) {
         let errorName = 'Error';
         if (error && typeof error === 'object' && 'name' in error) {
-            errorName = (error as { name: string }).name;
+            errorName = error.name;
         }
         const errorResponse = {
             message: 'Something went wrong',
@@ -78,179 +74,139 @@ booksRouters.get('/api/books', async (req: Request, res: Response) => {
             error: {
                 name: errorName,
                 errors: error
-
             }
         };
-        res.json(errorResponse)
+        res.json(errorResponse);
     }
-
-
-})
-
-
+}));
 // get signle book by id
-booksRouters.get('/api/books/:bookId', async (req: Request, res: Response) => {
+exports.booksRouters.get('/api/books/:bookId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const id = req.params.bookId;
-
-        const book = await booksCollections.findById(id);
+        const book = yield booksControlars_1.booksCollections.findById(id);
         res.json({
             success: true,
             message: "Book retrieved successfully",
             data: book
-        })
-
-    } catch (error) {
-
+        });
+    }
+    catch (error) {
         let errorName = 'Error';
         if (error && typeof error === 'object' && 'name' in error) {
-            errorName = (error as { name: string }).name;
+            errorName = error.name;
         }
-
         const errorResponse = {
             message: 'Something went wrong',
             success: false,
             error: {
                 name: errorName,
                 errors: error
-
             }
         };
-
-        res.json(errorResponse)
+        res.json(errorResponse);
     }
-
-
-})
-
-
+}));
 // updated single book
-booksRouters.put('/api/books/:bookId', async (req: Request, res: Response) => {
+exports.booksRouters.put('/api/books/:bookId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const id = req.params.bookId;
         const body = req.body;
         //   console.log(body)
-        const book = await booksCollections.findByIdAndUpdate(id, body, { new: true })
+        const book = yield booksControlars_1.booksCollections.findByIdAndUpdate(id, body, { new: true });
         res.status(201).json({
             success: true,
             message: "Book updated successfully",
             data: book
-        })
-
-    } catch (error) {
-
+        });
+    }
+    catch (error) {
         let errorName = 'Error';
         if (error && typeof error === 'object' && 'name' in error) {
-            errorName = (error as { name: string }).name;
+            errorName = error.name;
         }
-
         const errorResponse = {
             message: 'Something went wrong',
             success: false,
             error: {
                 name: errorName,
                 errors: error
-
             }
         };
-        res.json(errorResponse)
+        res.json(errorResponse);
     }
-
-})
-
-
+}));
 // delete single book
-booksRouters.delete('/api/books/:bookId', async (req: Request, res: Response) => {
+exports.booksRouters.delete('/api/books/:bookId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const id = req.params.bookId;
-
-        const book = await booksCollections.findByIdAndDelete(id)
+        const book = yield booksControlars_1.booksCollections.findByIdAndDelete(id);
         res.status(201).json({
             success: true,
             message: "Book deleted successfully",
             data: null
-        })
-    } catch (error) {
-
+        });
+    }
+    catch (error) {
         let errorName = 'Error';
         if (error && typeof error === 'object' && 'name' in error) {
-            errorName = (error as { name: string }).name;
+            errorName = error.name;
         }
-
         const errorResponse = {
             message: 'Something went wrong',
             success: false,
             error: {
                 name: errorName,
                 errors: error
-
             }
         };
-        res.json(errorResponse)
+        res.json(errorResponse);
     }
-})
-
-
-borrowRouters.post('/api/borrow', async (req: Request, res: Response) => {
+}));
+exports.borrowRouters.post('/api/borrow', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { book, quantity, dueDate } = req.body;
-
-        const singleBook = await booksCollections.findById(book);
-
+        const singleBook = yield booksControlars_1.booksCollections.findById(book);
         if (!singleBook) {
             return res.status(404).json({ message: "Book not found" });
         }
-
         if (singleBook.copies < quantity) {
             return res.status(400).json({ message: "Not enough copies available to borrow" });
         }
-
         singleBook.copies -= quantity;
-
         if (singleBook.copies === 0 && singleBook.available != false) {
             singleBook.available = false;
-            await singleBook.save()
+            yield singleBook.save();
         }
-
-        await singleBook.save();
-
-
-        const borrowBook = await borrowCollection.create({ book, quantity, dueDate });
-
-
+        yield singleBook.save();
+        const borrowBook = yield borrowControlar_1.borrowCollection.create({ book, quantity, dueDate });
         res.status(201).json({
             success: true,
             message: "Book borrowed successfully",
             data: borrowBook
         });
-    } catch (error) {
-
+    }
+    catch (error) {
         let errorName = 'Error';
         if (error && typeof error === 'object' && 'name' in error) {
-            errorName = (error as { name: string }).name;
+            errorName = error.name;
         }
-
         const errorResponse = {
             message: 'Something went wrong',
             success: false,
             error: {
                 name: errorName,
                 errors: error
-
             }
         };
-        res.json(errorResponse)
+        res.json(errorResponse);
     }
-});
-
-
-borrowRouters.get('/api/borrow', async (req: Request, res: Response) => {
+}));
+exports.borrowRouters.get('/api/borrow', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const result = await borrowCollection.aggregate([
+        const result = yield borrowControlar_1.borrowCollection.aggregate([
             {
                 $group: { _id: "$book", totalQuantity: { $sum: "$quantity" } }
             },
-
             {
                 $lookup: {
                     from: "bookscollections",
@@ -270,34 +226,26 @@ borrowRouters.get('/api/borrow', async (req: Request, res: Response) => {
                     totalQuantity: "$totalQuantity"
                 }
             }
-
-        ])
+        ]);
         res.status(201).json({
             success: true,
             message: "Book borrowed successfully",
             data: result
         });
-
-
-    } catch (error) {
-
+    }
+    catch (error) {
         let errorName = 'Error';
         if (error && typeof error === 'object' && 'name' in error) {
-            errorName = (error as { name: string }).name;
+            errorName = error.name;
         }
-
         const errorResponse = {
             message: 'Something went wrong',
             success: false,
             error: {
                 name: errorName,
                 errors: error
-
             }
         };
-        res.json(errorResponse)
+        res.json(errorResponse);
     }
-})
-
-
-
+}));
